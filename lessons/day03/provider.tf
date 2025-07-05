2226782c280fc3ab697b9854ae2f442bb8c86bcc
saqibjmi <mohdsaqibsky@gmail.com>
2025-07-05 16:44:29 +0530

    update provider.tf

--- lessons/day03/provider.tf
+++ lessons/day03/provider.tf
@@ -2,9 +2,17 @@ terraform {
   required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
-        version = "~> 4.8.0"
+        version = ">= 4.35.0"
     }
   }
+# in the below azure rm backend , i as a user have access onto the below backend storage account 
+  backend "azurerm" {
+    azurerm_resource_group = "hagahajak"  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    storage_account_name = "abcd1234"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+
   required_version = ">=1.9.0"
 }
 
@@ -15,5 +23,11 @@ provider "azurerm" {
       purge_soft_delete_on_destroy    = true
       recover_soft_deleted_key_vaults = true
     }
-}
+   }
+
+# to establish the connection between azurerm provider and terraform.
+   client_id       = "00000000-0000-0000-0000-000000000000"
+   client_secret   = var.client_secret
+   tenant_id       = "10000000-0000-0000-0000-000000000000"
+   subscription_id = "20000000-0000-0000-0000-000000000000"
 }
